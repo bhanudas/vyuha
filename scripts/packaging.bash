703bda221f291393a0a5d1345d4e00dc5b418337
Bhanudas Tanaka <bhanudas108@gmail.com>
2022-03-23 06:12:13 -1000

    adding support for third where clause

--- scripts/packaging.bash
+++ scripts/packaging.bash
@@ -1,3 +1,3 @@
 sfdx force:package:create --name VyuhaForSpire -d "Vyuha Bulk Flow System"  -t Unlocked --path force-app --targetdevhubusername pmfprod -e
 sfdx force:package:version:create --package "VyuhaForSpire" --definitionfile config/project-package-def.json --wait 10 -x --targetdevhubusername pmfprod
-sfdx force:package:install --package 04t5c0000014PoPAAU -u spiredev
+sfdx force:package:install --package 04t5c0000014QXzAAM -u spiredev
